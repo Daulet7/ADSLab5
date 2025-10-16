1428f79ee64e637bb8c812ca8ac40bdf11b5b9ea
Daulet7 <d_aitbay@kbtu.kz>
2025-10-16 18:47:03 +0500

    Add files via upload

---
+++ lab5b.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class heap{
+    public:
+    vector<long long> a;
+
+    long long size(){
+        return a.size();
+    }
+
+    long long parent(long long i){
+        return (i-1)/2;
+    }
+
+    long long left(long long i){
+        return i*2 + 1;
+    }
+
+    long long right(long long i){
+        return i*2+2;
+    }
+
+    long long getmax(){
+        return a[0];
+    }
+
+    void insert(long long k){
+        a.push_back(k);
+        long long index = a.size()-1;
+        while(index>0 && a[index]>a[parent(index)]){
+            swap(a[index], a[parent(index)]);
+            index = parent(index);
+        }
+    }
+
+    void heapify(long long i){
+        if(left(i) > a.size() - 1){
+            return;
+        }
+        long long j = left(i);
+        if(right(i)<a.size() && a[right(i)] > a[left(i)]){
+            j = right(i);
+        }
+        if(a[j]>a[i]){
+            swap(a[j], a[i]);
+            heapify(j);
+        }
+    }
+
+    long long pop_max(){
+        long long v = a[0];
+        swap(a[0], a[a.size()-1]);
+        a.pop_back();
+        if(!a.empty()){
+            heapify(0);
+        }
+        return v;
+    }
+};
+
+int main(){
+    long long n;
+    cin >> n;
+    heap arr;
+    for(long long i = 0; i<n; i++){
+        long long a;
+        cin >> a;
+        arr.insert(a);
+    }
+    while(arr.size()>1){
+        long long a;
+        long long b;
+        long long s;
+        a = arr.pop_max();
+        b = arr.pop_max();
+        s = a-b;
+        if(s!=0){
+            arr.insert(s);
+        }
+    }
+    if(arr.size()!=0){
+        cout << arr.pop_max();
+    }
+    else{
+        cout << 0;
+    }
+    return 0;
+}
